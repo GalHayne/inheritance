30a6f421871746b1302c51385be9c21f625da74e
Gal <galhayne@gmail.com>
2022-11-11 19:56:54 +0200

    add perimter function

--- script.js
+++ script.js
@@ -8,6 +8,10 @@ class Polygon {
     area() {
         console.log(`The area of: ${this.name} = ${this.height * this.width}`);
     }
+
+    perimeter(){
+        console.log(`The perimeter of: ${this.name} = ${this.height*2 + this.width*2}`);
+    }
 }
 
 class Square extends Polygon {
@@ -18,22 +22,35 @@ class Square extends Polygon {
 }
 
 class Triangle extends Polygon{
-    constructor(height, high){
-        super(height,high);
-        this. name = "Triangle"
+    constructor(side1, side2,side3,high){
+        super(side1,side2);
+        this.name = "Triangle"
+        this.height = high;
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
     }
 
     area(){
-        console.log(`The area of: ${this.name} = ${this.height * this.width}`);
+        console.log(`The area of: ${this.name} = ${this.height * this.side1 / 2}`);
+    }
+
+    perimeter(){
+        console.log(`The perimeter of: ${this.name} = ${this.side1 + this.side2 + this.side3}`);
     }
 }
 
 let polygon = new Polygon(11, 12);
 polygon.area();
+polygon.perimeter();
 
 let square = new Square(11);
-square.area(); //extends polygon say function
+square.area(); //use polygon say function
+square.perimeter();
 
-let triangle = new Triangle(10,5);
-triangle.area();
+let triangle = new Triangle(10,10,5,3);
+triangle.area(); //extends polygon say function and overload the function
+triangle.perimeter();
+
+;
 
